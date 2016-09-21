dcca62e9340ce95fec3ea6b6ba49c39443e1d969
Jason Wilcox <jason.wilcox@gmail.com>
2016-09-21 16:05:27 -0600

    Updated Dojo Survey to include server side validation

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash, redirect
 
 app = Flask(__name__)
+app.secret_key = 'ShhhSneaky'
 
 @app.route('/')
 def index():
@@ -8,12 +9,18 @@ def index():
 
 @app.route('/result', methods=['POST'])
 def result():
-    #Put Code Here
-    name = request.form['fullName']
-    location = request.form['location']
-    language = request.form['language']
-    comments = request.form['comments']
+    if len(request.form['fullName']) < 1:
+        flash("Name cannot be empty")
+        return redirect('/')
+    elif len(request.form['comments']) < 1 or len(request.form['comments']) > 120:
+        flash("Comments aren't actually optional but keep it under 120 characters!")
+        return redirect('/')
+    else:
+        name = request.form['fullName']
+        location = request.form['location']
+        language = request.form['language']
+        comments = request.form['comments']
+        return render_template("result.html", name=name, location=location, language=language, comments=comments)
 
-    return render_template("result.html", name=name, location=location, language=language, comments=comments)
 
 app.run(debug=True)
